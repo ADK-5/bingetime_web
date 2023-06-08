86d0d6e58d90165a390e84a08f456809fe0f2079
ADK-5 <100706262+ADK-5@users.noreply.github.com>
2023-06-08 15:04:16 +0530

    font changes

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL=BASE_URL + '/search/movie?'+API_KEY;
 
 const main=document.getElementById('main');
+const form=document.getElementById('form');
+const search= document.getElementById('search');
+const button=document.getElementById('searchbutton');
 
 getMovies(API_URL);
-
 function getMovies(url) {
     fetch(url).then(res => res.json()).then(data=>{
        showMovies(data.results);
@@ -22,12 +24,13 @@ function showMovies(data){
         const movieEl=document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML=`
+        <div class="eachmovie">
         <img src="${IMG_URL+poster_path}" alt="${title}">   
         <div class="movie-info">   
             <h3>${title}</h3>
             <span class="${getColor(vote_average)}">${vote_average}</span>
         </div>
-
+        </div>
         `
         main.appendChild(movieEl);
     });
@@ -42,3 +45,15 @@ function getColor(vote){
         return "red";
     }
 }
+
+searchbutton.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const searchTerm=search.value;
+
+    if(searchTerm){
+        getMovies(searchURL+'/query='+searchTerm);
+    }
+    else{
+        getMovies(API_URL);
+    }
+}) 
