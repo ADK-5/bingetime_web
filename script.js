652e1237c7d86b743eb63d861d8c86eecc461753
ADK-5 <100706262+ADK-5@users.noreply.github.com>
2023-06-09 00:13:41 +0530

    search button updated

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const API_KEY='api_key=3655ee9c7ada9894390d9da7a47b4968';
 const BASE_URL='https://api.themoviedb.org/3';
 const API_URL= BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL=BASE_URL + '/search/movie?'+API_KEY;
+const searchURL=BASE_URL + '/search/movie?query=';
 
 const main=document.getElementById('main');
 const form=document.getElementById('form');
@@ -10,6 +10,7 @@ const search= document.getElementById('search');
 const button=document.getElementById('searchbutton');
 
 getMovies(API_URL);
+
 function getMovies(url) {
     fetch(url).then(res => res.json()).then(data=>{
        showMovies(data.results);
@@ -51,7 +52,7 @@ searchbutton.addEventListener('click',(e)=>{
     const searchTerm=search.value;
 
     if(searchTerm){
-        getMovies(searchURL+'/query='+searchTerm);
+        getMovies(searchURL+searchTerm+'&'+API_KEY);
     }
     else{
         getMovies(API_URL);
